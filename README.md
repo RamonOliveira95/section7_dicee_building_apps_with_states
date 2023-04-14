4f0d0ec19cf62984013277c50be152652dfef6d3
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2023-04-14 16:02:42 -0300

    1.0.2

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 # AULA 2 - 1.0.1
 - Adicionado um Center envolvendo a Row principal, fazendo com que os dados fiquem centralizados;
 - Em cada Image foi adicionado um Padding de 16px para criar um espaçamento entre eles.
+
+# AULA 3 - 1.0.2
+- Retirado o Padding de cada image e adicionado um TextButton no lugar de cada um;
+- Em cada TextButton foi adicionado um print informando que aquele botão foi pressionado.
--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Botão da esquerda pressionado');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: TextButton(
+              onPressed: () {
+                print('Botão da direita pressionado');
+              },
               child: Image.asset('images/dice2.png'),
             ),
           ),
